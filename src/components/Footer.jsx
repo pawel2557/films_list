bf1b8f58a7866ff56e82792b06d2aa7ba433ba2f
pawel2557 <pawelnowak984@gmail.com>
2025-04-25 12:40:22 +0200

    added cards with films from api

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => {
     return (
-        <footer className="bg-white shadow-sm m-4 dark:bg-gray-800">
+        <footer className="bg-white border-t-2 border-gray-200 shadow-sm dark:bg-gray-800">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                 <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()} <Link to="/" className="hover:underline">Films list™</Link>. All Rights Reserved.
                 </span>
