0f6717ced630959ed84449b8287cd255b8c8c98e
pawel2557 <pawelnowak984@gmail.com>
2025-04-25 13:45:44 +0200

    added custom hook

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,17 +1,7 @@
-import { Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 import { Card } from "../components";
-import { useEffect, useState } from "react";
-export const MovieList = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    async function fetchMovies() {
-      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=cde1df3cbb5c3fdb4a87c0daf29197e3");
-      const data = await response.json();
-      setMovies(data.results)
-    }
-    fetchMovies();
-  }, [])
+export const MovieList = ({apiPath}) => {
+  const { data: movies } = useFetch(apiPath);
 
   return (
     <main>
