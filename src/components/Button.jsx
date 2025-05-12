424523e14044cf8d12dfbcac92984351cd8fa1b1
pawel2557 <pawelnowak984@gmail.com>
2025-05-12 23:51:23 +0200

    added page not found

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Button = ({children}) => {
+  return (
+<button className="w-64 text-xl bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 rounded-lg px-5 py-2.5 mr-2 mb-5 font-medium hover:cursor-pointer">{children}</button>
+  )
+}
