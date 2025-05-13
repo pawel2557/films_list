3819a6d2ea09074dbfb7a62aa6967cfe3af5321e
pawel2557 <pawelnowak984@gmail.com>
2025-05-13 10:53:08 +0200

    added movie detail page

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+import { ScrollToTop } from './components/index.jsx'
+
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
-    
+      <ScrollToTop></ScrollToTop>
       <App />
     </Router>
     
